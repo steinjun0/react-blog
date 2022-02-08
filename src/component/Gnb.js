359501b3063c11fc77a50df74d75f2557ad4d0d3
steinjun0 <1142308@naver.com>
2022-02-08 18:01:26 +0900

    react-blog git init

---
+++ src/component/Gnb.js
@@ -0,0 +1,40 @@
+// import styled from "styled-components";
+import { styled } from "@mui/material/styles";
+import { Box } from "@mui/material";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import InstagramIcon from "@mui/icons-material/Instagram";
+function Gnb() {
+  const GnbCommon = styled(Box)`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    background-color: black;
+    color: white;
+    height: 86px;
+  `;
+  const GnbTitle = styled("h1")`
+    margin-left: 16px;
+    font-size: 36px;
+  `;
+  const GnbIconGroup = styled(Box)`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: center;
+    width: 100px;
+    // margin-right: 16px;
+  `;
+  return (
+    <div>
+      <GnbCommon>
+        <GnbTitle>Stein.log</GnbTitle>
+        <GnbIconGroup>
+          <GitHubIcon></GitHubIcon>
+          <InstagramIcon></InstagramIcon>
+        </GnbIconGroup>
+      </GnbCommon>
+    </div>
+  );
+}
+export default Gnb;
