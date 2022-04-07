fd5e2cb5eeba854f1edfaa9480e03e2e2fe7b9e7
steinjun0 <1142308@naver.com>
2022-04-07 09:06:48 +0000

    get post data from api

--- src/component/PostCarousel/PostCarousel.js
+++ src/component/PostCarousel/PostCarousel.js
@@ -6,6 +6,7 @@ import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 
 import PostCarouselShowBlocks from "./PostCarouselBlocks";
+import API from 'API.js'
 
 // css-in-js
 const PostCarouselWrapper = styled("div")`
@@ -46,7 +47,7 @@ const PostCarouselCategory = styled("span")`
   justify-content: center;
   transform: translateX(
     ${({ category_index, post_carousels }) =>
-      post_carousels.length > 0 && -category_index * 100}%
+    post_carousels.length > 0 && -category_index * 100}%
   );
 `;
 
@@ -81,58 +82,75 @@ function PostCarousel({ categoryIndex, setCategoryIndex }) {
 
   useEffect(() => {
     // To do... getMainPagePosts API
-    setPostCarousels([
-      {
-        category: "Programming",
-        posts: [
-          {
-            categories: ["Django", "Docker"],
-            title: "gunicorn에 vscode debugger 붙이기",
-            subTitle: "개발자여, 조금 더 편하게 살아보자",
-          },
-          {
-            categories: ["Vue2"],
-            title: "v-model과 v-bind.sync 그리고 Vue3",
-            subTitle: "다가오는 Vue3와 변화되는 문법",
-          },
-          {
-            categories: ["Docker"],
-            title: "docker compose와 Monolothic Server 구축",
-            subTitle: "환경 구축으로부터의 탈출",
-          },
-          {
-            categories: ["Vue2"],
-            title: "v-model과 v-bind.sync 그리고 Vue3",
-            subTitle: "다가오는 Vue3와 변화되는 문법",
-          },
-        ],
-      },
-      {
-        category: "Camera",
-        posts: [
-          {
-            categories: ["Music", "Camera"],
-            title: "[자작곡] 프로필 스틸컷(2020)",
-            subTitle: "2020년 여름",
-          },
-          {
-            categories: ["Camera"],
-            title: "Trip for Europe: London",
-            subTitle: "2020/01/26 ~ 2020/01/30",
-          },
-          {
-            categories: ["Camera"],
-            title: "시험 전 휴식-농장",
-            subTitle: "2020/05/04",
-          },
-          {
-            categories: ["Music, Compose"],
-            title: "[Remake, Sample] 잘 있어요(아따맘마 오프닝)",
-            subTitle: "안녕하세요, 감사해요",
-          },
-        ],
-      },
-    ]);
+    const posts = API.getHomePostList();
+    posts.then(
+      (res) => {
+        if (res.status === 200) {
+          console.log('posts', res.data)
+          let temp = []
+          for (let category in res.data) {
+            temp.push({
+              category: category,
+              posts: res.data[category]
+            })
+          }
+          console.log(temp)
+          setPostCarousels(temp)
+        }
+      }
+    )
+    // setPostCarousels([
+    //   {
+    //     category: "Programming",
+    //     posts: [
+    //       {
+    //         categories: ["Django", "Docker"],
+    //         title: "gunicorn에 vscode debugger 붙이기",
+    //         subTitle: "개발자여, 조금 더 편하게 살아보자",
+    //       },
+    //       {
+    //         categories: ["Vue2"],
+    //         title: "v-model과 v-bind.sync 그리고 Vue3",
+    //         subTitle: "다가오는 Vue3와 변화되는 문법",
+    //       },
+    //       {
+    //         categories: ["Docker"],
+    //         title: "docker compose와 Monolothic Server 구축",
+    //         subTitle: "환경 구축으로부터의 탈출",
+    //       },
+    //       {
+    //         categories: ["Vue2"],
+    //         title: "v-model과 v-bind.sync 그리고 Vue3",
+    //         subTitle: "다가오는 Vue3와 변화되는 문법",
+    //       },
+    //     ],
+    //   },
+    //   {
+    //     category: "Camera",
+    //     posts: [
+    //       {
+    //         categories: ["Music", "Camera"],
+    //         title: "[자작곡] 프로필 스틸컷(2020)",
+    //         subTitle: "2020년 여름",
+    //       },
+    //       {
+    //         categories: ["Camera"],
+    //         title: "Trip for Europe: London",
+    //         subTitle: "2020/01/26 ~ 2020/01/30",
+    //       },
+    //       {
+    //         categories: ["Camera"],
+    //         title: "시험 전 휴식-농장",
+    //         subTitle: "2020/05/04",
+    //       },
+    //       {
+    //         categories: ["Music, Compose"],
+    //         title: "[Remake, Sample] 잘 있어요(아따맘마 오프닝)",
+    //         subTitle: "안녕하세요, 감사해요",
+    //       },
+    //     ],
+    //   },
+    // ]);
   }, []);
 
   return (
